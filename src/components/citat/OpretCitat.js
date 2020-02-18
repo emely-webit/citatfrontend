f36ccaf4f23231bca60faa76a99921849fedfd0f
Emely <emelybuchholt@gmail.com>
2020-02-18 14:08:23 +0100

    opret citat done
    Man kan nu oprette et citat og den bliver lagt i min database

--- src/components/citat/OpretCitat.js
+++ src/components/citat/OpretCitat.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
 function OpretCitat() {
+
+    const [opCitat, setOpCitat] = useState({})
+    const history = useHistory();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        let url = 'http://localhost:5009/citater/'
+        fetch(url, {
+            method: 'POST',
+
+            headers:{
+                'Content-Type': 'application/json',
+                
+            },
+            body: JSON.stringify(opCitat),
+        })
+        .then(function(data){
+            console.log(data);
+            alert("Dit citat er nu oprettet");
+            history.push("/citat_admin")
+        })
+        .catch(function(error){
+            alert("noget gik galt: " + error);
+        })
+    }
+
     return (
-        <div>
-            OpretCitat
+        <div className="container">
+            <h1 className="py-5">Opret et nyt citat</h1>
+            
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input name="overskrift" onChange={(e) => setOpCitat({...opCitat, titel: e.target.value})} type="text" className="form-control" placeholder="Citatets titel"/>
+                </div>
+                <div className="form-group">
+                    <textarea name="citattekst" rows="3" onChange={(e) => setOpCitat({...opCitat, citatTekst: e.target.value})} className="form-control" placeholder="Citatets tekst....."></textarea>
+                </div>
+                <div className="form-group">
+                    <textarea name="katId" rows="3" defaultValue="5e4a61d8b4ee133528713fd2" onChange={(e) => setOpCitat({...opCitat, kategoriId: e.target.value})} className="form-control" placeholder="Citatets tekst....." ></textarea>
+                </div>
+                <button type="button" onClick={() => {history.push("/citat_admin")}} className="btn btn-danger mr-3">Fortryd</button>
+                <button type="submit" className="btn btn-success">Gem Citat</button>
+            </form>
         </div>
     )
 }
